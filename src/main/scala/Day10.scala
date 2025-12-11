cc9b4f17df5cad8997eae712583697481687f8bb
Reiti <Reiti@users.noreply.github.com>
2025-12-11 13:11:05 +0100

    Day 10 P2 possible slow

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,6 +1,5 @@
 import util.{Day, Util}
 
-import java.awt.Button
 import scala.annotation.tailrec
 
 case class Machine(lights: List[Boolean], buttons: List[Set[Int]], joltage: List[Int]);
@@ -17,9 +16,7 @@ object Day10 extends Day(10):
     //println(machines.map(findAllOn).sum)
 
     //Part 2
-    machines.map(findJoltage).foreach(println)
-
-
+    println(machines.map(findJoltage).sum)
 
   def parseMachines(line: String): Machine =
     val split = line.split(" ")
@@ -54,17 +51,45 @@ object Day10 extends Day(10):
   def findMax(button: Set[Int], joltage: List[Int]): Int =
     LazyList.from(0).takeWhile(f => button.forall(b => f <= joltage(b))).last
 
+
+  def isValid(current: List[Int], joltage: List[Int]): Boolean =
+    current.zip(joltage).forall(z => z._1 <= z._2)
+
   def findJoltage(machine: Machine): Long =
-    def _find(remaining: List[Set[Int]], presses: Long, remainingJoltage: List[Int]): Long =
-      if remaining.isEmpty then
-        if remainingJoltage.forall(_ == 0) then
-          presses
-        else
-          Integer.MAX_VALUE
+    var currMin: Int = Integer.MAX_VALUE
+
+    println("next")
+    def _find(remainingButtons: List[Set[Int]], currentJoltage: List[Int], presses: Int): Long =
+      if presses >= currMin then
+        currMin
+      else if currentJoltage == machine.joltage then
+        currMin = presses
+        currMin
       else
-        val (d, min) = findLowestToMax(remaining, remainingJoltage)
+        val leastButtons = currentJoltage.indices.filter(i => currentJoltage(i) != machine.joltage(i)).minBy(c => remainingButtons.count(b => b.contains(c)))
+        val relevantButtons = remainingButtons.filter(b => b.contains(leastButtons))
+        val buttonCount = relevantButtons.size
+        val missingJoltage = machine.joltage(leastButtons) - currentJoltage(leastButtons)
 
-        val next = min.map(b => (remaining.filterNot(_ == b), remainingJoltage.zipWithIndex.map(z => if b.contains(z._2) then z._1 - d else z._1)))
-        next.map(n => _find(n._1, presses + d, n._2)).min
+//        remainingButtons foreach println
+//        println(leastButtons)
+//        println(missingJoltage)
+//        println("---------------")
+        if buttonCount == 0 then
+          Integer.MAX_VALUE
+        else
+          val possiblePresses = partitions(buttonCount, missingJoltage)
 
-    _find(machine.buttons, 0L, machine.joltage)
+          possiblePresses.map(p => pressButtons(relevantButtons, p, currentJoltage)).filter(j => isValid(j, machine.joltage)).map(j => _find(remainingButtons.filter(b => !b.contains(leastButtons)), j, presses + missingJoltage)).minOption.getOrElse(Integer.MAX_VALUE)
+
+    _find(machine.buttons, List.fill(machine.joltage.length)(0), 0)
+
+  def pressButtons(buttons: List[Set[Int]], presses: List[Int], currentJoltage: List[Int]): List[Int] = currentJoltage.indices.map(i => valueAt(i, buttons, presses)).toList.zip(currentJoltage).map(z => z._1 + z._2)
+
+  def valueAt(i: Int, buttons: List[Set[Int]], presses: List[Int]): Int = presses.zip(buttons).map(z => if z._2.contains(i) then z._1 else 0).sum
+
+  def partitions(count: Int, value: Int): List[List[Int]] =
+    if count == 1 then
+      List(List(value))
+    else
+      (0 to value).flatMap(v => partitions(count - 1, value - v).map(p => p.prepended(v))).toList
