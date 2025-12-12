172a72df3c8f4d083c15957bb29ad3f12569a972
Reiti <Reiti@users.noreply.github.com>
2025-12-12 08:58:00 +0100

    Day 12 even funnier

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -1,9 +1,6 @@
 import util.Day
-import util.Util.{x, y}
 
 object Day12 extends Day(12):
   override def solve(): Unit =
-    val boxes = inputLines.dropWhile(l => !l.contains('x')).map(_.trim).filter(l => !l.isBlank).map(b => b.split(":")).map(b => (b.head.trim.split("x"), b.last.trim.split(" "))).map(b => ((b._1.head.toInt, b._1.last.toInt), b._2.map(_.toInt)))
-
     //Part 1
-    println(boxes.count(b => (b._1.x * b._1.y) >= b._2.map(_ * 9).sum)) //lol, lmao even
+    println(inputLines.dropWhile(l => !l.contains('x')).map(b => b.split(":")).count(b => b.head.trim.split("x").map(_.toInt).product >= b.last.trim.split(" ").map(_.toInt).sum * 9)) //lol, lmao even
